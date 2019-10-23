0f4579acda43453da3f7903b75511e568d52de44
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-23 18:16:43 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Dima Dovgalyk on 2019-10-23.
+//  Copyright © 2019 Dima Dovgalyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
