75519b58aae583671e0e02110bd5db6344f95b56
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-24 10:34:01 -0400

    finished rectangle area helper. For some reason it doesn't work. Will think more about this

---
+++ AreaHelper/TriangleViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TriangleViewController.swift
+//  AreaHelper
+//
+//  Created by Dima Dovgalyk on 2019-10-24.
+//  Copyright © 2019 Dima Dovgalyk. All rights reserved.
+//
+
+import Foundation
